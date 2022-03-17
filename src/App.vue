b5d3239ef84f9d29ab6e33ea8e7b15dc298d3660
Shohin <kesamirov.071197@mail.ru>
2022-03-17 17:05:17 +0300

    add Pagination Component

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,45 @@
     <div class="mb-20">
       <Card v-for="image in imagesDataset" :key="image.id" :image="image" />
     </div>
-    <div
-      class="bg-black text-white h-16 flex items-center justify-center opacity-90 fixed left-0 right-0 bottom-0"
-    >
-      <button class="block">1</button>
-    </div>
+    <pagination @page="getPage" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import Card from "./components/Users/Card.vue";
+import Pagination from "./components/Pagination.vue";
 const API_KEY = "k6unMgPsTj1viPCTeUZgwAC0KTilbQk2ytB-wLcV_dU";
 export default {
   name: "App",
-  components: { Card },
+  components: { Card, Pagination },
   data() {
     return {
       imagesDataset: [],
+      currentPage: 5,
     };
   },
   mounted() {
-    this.getUserDataset();
+    this.getUserDataset(this.currentPage);
+  },
+  watch: {
+    currentPage(newValue) {
+      this.getUserDataset(newValue);
+    },
   },
   methods: {
-    async getUserDataset() {
+    async getUserDataset(page) {
       try {
         const res = await axios.get(
-          `https://api.unsplash.com/photos?client_id=${API_KEY}`
+          `https://api.unsplash.com/photos?page=${page}&client_id=${API_KEY}`
         );
         this.imagesDataset = res.data;
-        console.log(this.imagesDataset);
       } catch (error) {
         console.log(error);
       }
+    },
+    getPage(val) {
+      this.currentPage = val;
     },
   },
 };
---
+++ src/components/Pagination.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    class="bg-black text-white h-16 flex items-center justify-center opacity-90 fixed left-0 right-0 bottom-0 gap-x-3"
+  >
+    <div class="flex gap-x-3 text-xs">
+      <button @click="goToFirstpage">{{ firstPage }}</button>
+      <p>...</p>
+      <button v-if="currentPage !== firstPage" @click="goToPrevPage">
+        {{ currentPage - 1 }}
+      </button>
+    </div>
+    <button>{{ currentPage }}</button>
+    <div class="flex gap-x-3 text-xs">
+      <button v-if="currentPage !== lastPage" @click="goToNextPage">
+        {{ currentPage + 1 }}
+      </button>
+      <p>...</p>
+      <p @click="goToLastpage">{{ lastPage }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lastPage: 59,
+      firstPage: 1,
+      currentPage: 5,
+    };
+  },
+  methods: {
+    goToNextPage() {
+      if (this.currentPage < this.lastPage - 1) {
+        this.currentPage++;
+        this.$emit("page", this.currentPage);
+      }
+    },
+    goToPrevPage() {
+      if (this.currentPage > this.firstPage + 1) {
+        this.currentPage--;
+        this.$emit("page", this.currentPage);
+      }
+    },
+    goToFirstpage() {
+      this.currentPage = this.firstPage;
+      this.$emit("page", this.currentPage);
+    },
+    goToLastpage() {
+      this.currentPage = this.lastPage;
+      this.$emit("page", this.currentPage);
+    },
+  },
+};
+</script>
+
+<style></style>
